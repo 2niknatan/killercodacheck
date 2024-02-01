e73d2cb8023052b7078a4c5ccd62c9b68a750bfc
2nik67 <2niknatan@gmail.com>
2024-02-01 10:44:20 +0200

    123

--- KubiScan/background.sh
+++ KubiScan/background.sh
@@ -1,3 +1,38 @@
 cd /tmp
 git clone https://github.com/cyberark/KubiScan.git
+kubectl apply -f kubiscan-sa.yaml
+KUBISCAN_SA_SECRET=$(kubectl get sa kubiscan-sa -o=jsonpath='{.secrets[0].name}')
+kubectl apply -f - << EOF
+apiVersion: v1
+kind: Pod
+metadata:
+  name: mypod
+  namespace: default
+spec:
+  serviceAccountName: kubiscan-sa
+  containers:
+  - image: nginx
+    name: c1
+    volumeMounts:
+    - name: secret-volume
+      readOnly: true
+      mountPath: "/var/run/secrets/kubernetes.io/serviceaccount"
+    - name: not-token-secret
+      mountPath: "/var/run/secrets/tokens"
+  - image: nginx
+    name: c2
+  volumes:
+  - name: secret-volume
+    secret:
+      secretName: "${KUBISCAN_SA_SECRET}"
+  - name: not-token-secret
+    secret:
+      secretName: mysecret
+EOF
+
+kubectl apply -f my-pod-env.yaml
+kubectl apply -f pod-with-host-access.yaml
+
+
+
 
