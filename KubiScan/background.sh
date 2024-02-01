fa9a1fcc1d1865de6d14bb3ebc3c749d968eed5e
2nik67 <2niknatan@gmail.com>
2024-02-01 15:03:44 +0200

    123

--- KubiScan/background.sh
+++ KubiScan/background.sh
@@ -1,8 +1,5 @@
 cd /tmp
 git clone https://github.com/cyberark/KubiScan.git
-cd /tmp/KubiScan
-pip install -r requirements.txt
-alias kubiscan='python3 /tmp/KubiScan/KubiScan.py'
 kubectl apply -f - << EOF
 apiVersion: v1
 kind: Secret
@@ -72,7 +69,55 @@ spec:
     secret:
       secretName: mysecret
 EOF
-
+kubectl apply -f - << EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: my-secret
+type: Opaque
+data:
+  username: dXNlcm5hbWU=
+  password: cGFzc3dvcmQ=
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: my-pod-env
+spec:
+  containers:
+  - name: my-container
+    image: my-image
+    env:
+    - name: MY_USERNAME
+      valueFrom:
+        secretKeyRef:
+          name: my-secret
+          key: username
+    - name: MY_PASSWORD
+      valueFrom:
+        secretKeyRef:
+          name: my-secret
+          key: password
+EOF
+kubectl apply -f << EOF
+apiVersion: v1
+kind: Pod
+metadata:
+  name: pod-with-host-access-and-volume
+spec:
+  containers:
+  - name: host-access-container
+    image: your-image
+    securityContext:
+      privileged: true
+    volumeMounts:
+    - name: host-volume
+      mountPath: /host-data
+  volumes:
+  - name: host-volume
+    hostPath:
+      path: /path/on/host
+EOF
 
 
 
--- KubiScan/foreground.sh
+++ KubiScan/foreground.sh
@@ -1,1 +1,4 @@
+cd /tmp/KubiScan
+pip install -r requirements.txt
+alias kubiscan='python3 /tmp/KubiScan/KubiScan.py'
 kubiscan
