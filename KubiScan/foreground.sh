6d7422c761d5535e0de96b5df43d98126e61f3a6
2nik67 <2niknatan@gmail.com>
2024-02-06 18:13:47 +0200

    123

--- KubiScan/foreground.sh
+++ KubiScan/foreground.sh
@@ -1,5 +1,3 @@
-# cd /tmp/KubiScan
-# pip install -r requirements.txt
-# alias kubiscan='python3 /tmp/KubiScan/KubiScan.py'
-# clear
-# kubiscan
+alias kubiscan='python3 /tmp/KubiScan/KubiScan.py'
+clear
+kubiscan
