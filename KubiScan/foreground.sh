f606c260f8a6db2adf8757829f974f72d8811d05
2nik67 <2niknatan@gmail.com>
2024-02-07 11:35:30 +0200

    123

--- KubiScan/foreground.sh
+++ KubiScan/foreground.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 alias kubiscan='python3 /tmp/KubiScan/KubiScan.py'
-echo "KubiScan installing requirments..."; while [ ! -f /tmp/.backgroundfinished ] ; do sleep 2; done; echo "Done!"
+echo "KubiScan installing requirements..."; while [ ! -f /tmp/.backgroundfinished ] ; do sleep 1; done; echo "Done!"
 kubiscan
